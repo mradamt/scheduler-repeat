75915c328c4d24f5a158cbfba528def9b300f65e
Adam Tyler <mr.adamtyler@gmail.com>
2021-12-06 12:05:44 -0800

    refactor interviewerlistitem
    - remove state, state should be stored in the parent
    - add onClick function using props
    - remove hardcode data replace with props variables

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import classNames from "classnames";
 
 import 'components/InterviewerListItem.scss'
 
 export default function InterviewerListItem(props) {
-  // const {interviewer, setInterviewer} = useState("Betsy")
+  let interviewerClass = classNames(
+    "interviewers__item",
+    {"interviewers__item--selected": props.selected}
+  )
 
   return (
-    <li className="interviewers__item">
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}> 
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sally Nomates"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sally Nomates
+      {props.selected && props.name}
     </li>
   )
 }

5e84183fdb9ccff5ac71bae7de46f8cd6fff1dd4
Adam Tyler <mr.adamtyler@gmail.com>
2021-12-11 19:29:17 -0800

    add save & cancel functions
    - correct 'name' field of <input>
    - correct value & onChange so they work
    - prevent default form submission

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,16 +8,27 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "")
   const [interviewer, setInterviewer] = useState(props.interviewer || null)
 
+  function reset() {
+    setStudent("")
+    setInterviewer(null)
+  }
+  
+  function cancel() {
+    reset()
+    props.onCancel()
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name={student}
+            name="name"
             type="text"
             placeholder="Enter Student Name"
-            onChange={(value) => setStudent(value)}
+            value={student}
+            onChange={event => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList 
@@ -28,8 +39,8 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
         </section>
       </section>
     </main>

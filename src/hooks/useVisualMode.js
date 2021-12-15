22e91bd4a640aa4c50d96742b8cc58aeb75cc11d
Adam Tyler <mr.adamtyler@gmail.com>
2021-12-15 14:32:55 -0800

    add history state, write back() logic

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,22 @@ import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial])
 
-  const transition = x => setMode(x)
+  const transition = newMode => {
+    setMode(newMode)
+    const updated = [...history]
+    updated.push(newMode)
+    setHistory(updated)
+  }
 
-  const back = x => x
+  const back = () => {
+    const updated = [...history]
+    updated.pop()
+    setHistory(updated)
+    const prev = updated[updated.length - 1]
+    setMode(prev)
+  }
 
   return {
     mode,

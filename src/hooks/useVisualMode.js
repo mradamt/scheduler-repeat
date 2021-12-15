3481eb57e862b59b7f0ccc1b3ee215e2b166604f
Adam Tyler <mr.adamtyler@gmail.com>
2021-12-15 14:06:55 -0800

    (partial) write back() func with test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,9 +4,12 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
   const transition = x => setMode(x)
-  
+
+  const back = x => x
+
   return {
     mode,
-    transition
+    transition,
+    back
   }
 }

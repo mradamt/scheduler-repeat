7c25df1075db1695e34520b0b8ed99da78aac425
Adam Tyler <mr.adamtyler@gmail.com>
2021-12-16 13:15:17 -0800

    refactor selectors, trim code

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,19 +1,11 @@
 export function getAppointmentsForDay(state, day) {
-  let keys = []
-  
-  for (const dayObj of state.days) {
-    if (dayObj.name === day) {
-      keys = dayObj.appointments
-    }
-  }
-
-  return keys.map(id => state.appointments[id])
+  const dayObj = state.days.find(dayObj => dayObj.name === day)
+  if (!dayObj) return []
+  return dayObj.appointments.map(id => state.appointments[id])
 }
 
 export function getInterviewersForDay(state, day) {
-  const foundDay = state.days.find(dayObj => dayObj.name === day)
-
-  if (!foundDay) return []
-
-  return foundDay.interviewers.map(id => state.interviewers[id])
+  const dayObj = state.days.find(dayObj => dayObj.name === day)
+  if (!dayObj) return []
+  return dayObj.interviewers.map(id => state.interviewers[id])
 }
